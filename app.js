e48321d08be0b904ab194f4094fc7d257f080a3f
Arthur Borges <arthurborges.dev@gmail.com>
2018-05-09 14:19:04 -0300

    Added blinkage part

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+var ledToggle;
+var led = require("pi-pins").connect(47);
+
+led.mode('out');
+led.value(true);
+
+setTimeout(function() {
+  ledToggle = !ledToggle;
+  led.value(ledToggle);
+}, 1000)
