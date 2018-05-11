31f50fe2445d935c6eba30edd23815c99d953b78
Arthur Borges <arthurborges.dev@gmail.com>
2018-05-11 17:32:55 -0300

    Added toggle to led

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 console.log("Hello World!");
 
-var Gpio = require('pigpio').Gpio,
-  led = new Gpio(47, {mode: Gpio.OUTPUT}),
-  dutyCycle = 0;
+var Gpio = require('pigpio').Gpio;
+var led = new Gpio(47, { mode: Gpio.OUTPUT });
+var toggle = true;
 
 setInterval(function () {
-  led.pwmWrite(dutyCycle);
-
-  dutyCycle += 5;
-  if (dutyCycle > 255) {
-    dutyCycle = 0;
-  }
-}, 20);
+  toggle = !toggle;
+  led.digitalWrite(toggle);
+}, 1000);
