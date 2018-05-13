d8c0301385df0ad146a6e01b3b2d92fd0d2761e9
Arthur Borges <arthurborges.dev@gmail.com>
2018-05-13 19:25:56 -0300

    Added button page

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+// server
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// botao
+var Gpio = require('pigpio').Gpio;
+var led = new Gpio(47, { mode: Gpio.OUTPUT });
+
+app.get('/botao', function (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+  console.log("Acessou a página do botão");
+});
+
 console.log("Hello World!");
 
 var Gpio = require('pigpio').Gpio;
@@ -8,3 +22,5 @@ setInterval(function () {
   toggle = !toggle;
   led.digitalWrite(toggle);
 }, 1000);
+
+app.listen(80, () => console.log('Example app listening on port 80!'))
